43989d9d080b91949a605ab6d60a8361383381c6
Luiz_Guilherme <luiz.2022310970@aluno.iffar.edu.br>
2024-03-11 16:39:35 -0300

    Update alterar.php

--- alterar.php
+++ alterar.php
@@ -5,15 +5,13 @@ include("conecta.php");
 
 // receber os dados do formulário
 $id = $_POST['id'];
-$texto = $_POST['texto'];
-$imagem = $_POST['imagem'];
-$escolha1 = $_POST['escolha1'];
-$texto1 = $_POST['texto1'];
-$escolha2 = $_POST['escolha2'];
-$texto2 = $_POST['texto2'];
+$nome = $_POST['nome'];
+$id_produto = $_POST['idproduto'];
+
+
 
 $sql = "UPDATE historia SET id_historia = $id, 
-texto = '$texto', nome_imagem = '$imagem', escolha1 = $escolha1, escolha2 = $escolha2, texto1 = '$texto1', texto2 = '$texto2' WHERE id_historia = $id";
+WHERE idproduto = $id_produto";
 
 // executa o comando no BD
 mysqli_query($conexao,$sql);
