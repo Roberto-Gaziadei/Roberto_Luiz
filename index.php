353b10e3bd457aacced8a0356da100ccdc10922c
Roberto_Graziadei <roberto.2022315930@aluno.iffar.edu.br>
2024-03-11 16:50:14 -0300

    Update index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crud</title>
+</head>
+<body>
+    <h1>Página Inicial do Crud</h1><br>
+    <a href="cadastrar.php">Cadastrar</a><br>
+    <a href="alterar.php">Alterar</a><br>
+    <a href="listar.php">Listar</a><br>
+    <a href="excluir.php">Excluir</a><br>
+</body>
+</html>
